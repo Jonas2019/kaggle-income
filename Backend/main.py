8c154c2bd934d3f4af6b81aa0a62766b1ac67dd9
Jonas_QD <richmondjonas@gmail.com>
2022-01-12 00:25:05 -0800

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -3,8 +3,11 @@ from fastapi import FastAPI
 from pymongo import MongoClient
 import pandas as pd
 
+mongoURI = "[Enter URI Here]"
+mongoURI = "mongodb+srv://dbAdmin:cmpt732@cluster732.jfbfw.mongodb.net"
+
 app = FastAPI()
-client = MongoClient("mongodb+srv://dbAdmin:cmpt732@cluster732.jfbfw.mongodb.net")
+client = MongoClient(mongoURI)
 db = client.KaggleProject
 records = list(db["Income"].find())
 print(type(records))
